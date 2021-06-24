b650383b74e2b7abb3937810d0aedf2373bd1ada
RomanHyde <romanhyde1@gmail.com>
2021-06-24 15:24:52 +0100

    fix/ user js typo

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 // defines our user schema
 const mongoose = require('mongoose');
 
-const user = new.mongoose.Schema({
+const user = new mongoose.Schema({
     name: {
         type: String, 
         required: true
