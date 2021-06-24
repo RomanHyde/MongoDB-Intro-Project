d45c7b690b1f9164a9091721c04de5715dfe848c
RomanHyde <romanhyde1@gmail.com>
2021-06-24 15:55:58 +0100

    add get users with async await user.find

--- routes.js
+++ routes.js
@@ -2,11 +2,14 @@ const express = require('express');
 const router = express.Router();
 const User = require('./models/user');
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    const users = await User.find({});
     // res.status(200).render('login');
-    res.status(200).send('Hello World!');
+    // res.status(200).send('Hello World!');
+    res.status(200).json(users);
 });
 
+// post user first and then confirm
 router.post('/',(req,res) => {
     let name = req.body.name;
     let email = req.body.email;
